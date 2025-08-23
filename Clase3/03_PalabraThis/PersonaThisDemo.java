b06bb10492366edd881b240d82a4e5ec99cb2a8c
felixherrera2196 <felixherrera2196@gmail.com>
2025-08-22 18:18:18 -0600

    refactor: separar demos y simplificar ejemplos de la clase 3

---
+++ Clase3/03_PalabraThis/PersonaThisDemo.java
@@ -0,0 +1,6 @@
+public class PersonaThisDemo {
+    public static void main(String[] args) {
+        PersonaThis p = new PersonaThis("Luis");
+        p.saludar();
+    }
+}
---
+++ Clase3/04_Constructores/PuntoDemo.java
@@ -0,0 +1,6 @@
+public class PuntoDemo {
+    public static void main(String[] args) {
+        Punto p = new Punto(3, 4);
+        System.out.println("Punto: (" + p.getX() + ", " + p.getY() + ")");
+    }
+}
